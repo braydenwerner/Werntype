80e35c794e565f213cd7129a0017ce595187516c
Brayden Werner <braydenwerner0@gmail.com>
2020-12-16 22:27:02 -0800

    refactor keyboard component

--- src/Components/Keyboard/Keyboard.js
+++ src/Components/Keyboard/Keyboard.js
@@ -1,14 +1,14 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import './Keyboard.scss'
 
 export default function Keyboard() {
-    const keyRefs = []
-    const rows = []
-    rows.push(['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']'])
-    rows.push(['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\''])
-    rows.push(['z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/'])
+    const rows = [
+        ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']'],
+        ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\''],
+        ['z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/']
+    ]
 
-    let keyIndex = -1
+    const [keysToHighlight, setKeysToHighlight] = useState(new Set())
 
     useEffect(() => {
         window.addEventListener('keydown', handleKeyDown)
@@ -17,40 +17,39 @@ export default function Keyboard() {
     }, [])
 
     const handleKeyDown = (e) => {
-        let currentKey = -1
-        rows.forEach((row) => {
-            row.forEach((key) => {
-                currentKey++
-                if (e.key === key) {
-                    console.log(key)
-                    const keyElement = keyRefs[currentKey].current
-                    keyElement.style.backgroundColor = 'Red'
-                    setTimeout(function () { keyElement.style.backgroundColor = 'White' }, 100)
-                }
-            })
+        setKeysToHighlight((oldKeysToHighlight) => {
+            const newKeysToHighlight = new Set(oldKeysToHighlight)
+            newKeysToHighlight.add(e.key)
+            return newKeysToHighlight
         })
+
+        setTimeout(function () {
+            setKeysToHighlight((oldKeysToHighlight) => {
+                const newKeysToHighlight = new Set(oldKeysToHighlight)
+                newKeysToHighlight.delete(e.key)
+                return newKeysToHighlight
+            })
+        }, 100)
+
+        console.log(keysToHighlight)
     }
 
     return (
-        <>
-            <div id="keyboard-outer-container">
-                {rows.map((row) => {
-                    return (
-                        <div key={row} id={`row-${row}`} className="key-row">
-                            {row.map((key) => {
-                                keyIndex++
-                                keyRefs.push(React.createRef())
-                                return (
-                                    //  set each div to its corresponding reference
-                                    <div className="key-container" key={key} ref={keyRefs[keyIndex]}>
-                                        <span className="key">{key}</span>
-                                    </div>
-                                )
-                            })}
-                        </div>
-                    )
-                })}
-            </div>
-        </>
+        <div id="keyboard-outer-container">
+            {rows.map((row) => {
+                return (
+                    <div className="key-row" key={row}>
+                        {row.map((key) => {
+                            console.log(`key-highlight-${keysToHighlight.has(key)}`)
+                            return (
+                                <div className={`key-highlight-${keysToHighlight.has(key)}`} key={key}>
+                                    <span className="key">{key}</span>
+                                </div>
+                            )
+                        })}
+                    </div>
+                )
+            })}
+        </div>
     )
 }
