f01c7cd7319274891eee840a64d86d788035957f
Brayden Werner <braydenwerner0@gmail.com>
2020-12-19 22:36:59 -0800

    started auth

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,12 +1,33 @@
 import React, { useEffect } from 'react'
 import { useRecoilValue } from 'recoil'
 import { pageState } from '../../recoil'
+import { app, auth, provider } from '../../firebase'
 import './Profile.scss'
 
 export default function Profile() {
   const currentPageState = useRecoilValue(pageState)
 
-  useEffect(() => {}, [])
+  useEffect(() => {
+    auth
+      .signInWithPopup(provider)
+      .then(function (result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken
+        // The signed-in user info.
+        var user = result.user
+        // ...
+      })
+      .catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code
+        var errorMessage = error.message
+        // The email of the user's account used.
+        var email = error.email
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential
+        // ...
+      })
+  }, [])
 
   return (
     <>
--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,16 @@
 import firebase from 'firebase/app'
 import 'firebase/auth'
+import 'firebase/firestore'
 
-const firebase = firebase.initializeApp({
-  apiKey: REACT_APP_FIREBASE_API_KEY,
-  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: REACT_APP_FIREBASE_APP_ID,
-  measurementId: REACT_APP_FIREBASE_MEASUREMENT_ID
+export const app = firebase.initializeApp({
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 })
 
-export const database = firebase.firestore()
-export const auth = firebase.auth()
-export default firebase
+export const auth = app.auth()
+export const provider = new firebase.auth.GoogleAuthProvider()
