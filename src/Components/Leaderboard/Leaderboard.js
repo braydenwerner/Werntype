ce4bb2bb3b64dfe030f08e19adb68e815e61289e
Brayden Werner <braydenwerner0@gmail.com>
2020-12-24 11:28:01 -0800

    real time database listener

--- src/Components/Leaderboard/Leaderboard.js
+++ src/Components/Leaderboard/Leaderboard.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 import { db } from '../../firebase'
 import { pageState } from '../../atoms/atoms'
 import './Leaderboard.scss'
@@ -6,34 +6,77 @@ import { useRecoilValue } from 'recoil'
 import AnimatedHeader from '../AnimatedHeader/AnimatedHeader'
 
 export default function Leaderboard() {
+  const dropDownRef = useRef(null)
+
   const currentPageState = useRecoilValue(pageState)
+
   const [bestScores, setBestScores] = useState([])
+  const [sortBy, setSortBy] = useState('bestWPM')
+
   let isPrimary = true
 
   useEffect(() => {
-    const scores = []
-    db.collection('users')
-      .get()
-      .then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-          scores.push({
-            username: doc.data().username,
-            bestWPM: doc.data().bestWPM,
-            avgWPM: doc.data().avgWPM,
-            totalRaces: doc.data().totalRaces
-          })
+    const snapshot = db.collection('users').onSnapshot((querySnapshot) => {
+      const scores = []
+      querySnapshot.forEach((doc) => {
+        scores.push({
+          username: doc.data().username,
+          bestWPM: doc.data().bestWPM,
+          avgWPM: doc.data().avgWPM,
+          totalRaces: doc.data().totalRaces
         })
-
-        scores.sort((a, b) => b.bestWPM - a.bestWPM)
-        setBestScores(scores)
       })
+
+      if (sortBy === 'bestWPM') {
+        scores.sort((a, b) => b.bestWPM - a.bestWPM)
+      } else if (sortBy === 'averageWPM') {
+        scores.sort((a, b) => b.avgWPM - a.avgWPM)
+      } else if (sortBy === 'totalRaces') {
+        scores.sort((a, b) => b.totalRaces - a.totalRaces)
+      }
+
+      setBestScores(scores)
+    })
+
+    return () => snapshot()
   }, [])
+
+  useEffect(() => {
+    const scores = [...bestScores]
+
+    if (sortBy === 'bestWPM') {
+      scores.sort((a, b) => b.bestWPM - a.bestWPM)
+    } else if (sortBy === 'averageWPM') {
+      scores.sort((a, b) => b.avgWPM - a.avgWPM)
+    } else if (sortBy === 'totalRaces') {
+      scores.sort((a, b) => b.totalRaces - a.totalRaces)
+    }
+
+    setBestScores(scores)
+  }, [sortBy])
+
+  const handleDropdown = () => {
+    setSortBy(dropDownRef.current.value)
+  }
 
   return (
     <>
       {currentPageState === 'leaderboardState' && (
         <div id="leaderboard-outer-container">
           <AnimatedHeader text="Leaderboard" />
+          <div id="leaderboard-select-container">
+            <label id="leaderboard-label"> Sorting by</label>
+            <select
+              id="leaderboard-select"
+              name="leaderboard-select"
+              onChange={handleDropdown}
+              ref={dropDownRef}
+            >
+              <option value="bestWPM">Best WPM</option>
+              <option value="averageWPM">Average WPM</option>
+              <option value="totalRaces">Total Races</option>
+            </select>
+          </div>
           <table id="leaderboard-table" rules="none">
             <thead>
               <tr
